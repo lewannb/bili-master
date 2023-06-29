c266959fa8406a922e28f427ea8fc92a4f736ab0
xiao_lin <miain1590811@gmail.com>
2023-06-29 16:30:41 +0800

    first submit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+/*
+ * @Author: linxx
+ * @Date: 2023-06-29 14:33:44
+ * @LastEditTime: 2023-06-29 14:38:37
+ * @LastEditors: linxx
+ * @Description: 
+ * @FilePath: \page-bili\src\App.tsx
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+/*
+ * @Author: linxx
+ * @Date: 2023-06-29 14:33:44
+ * @LastEditTime: 2023-06-29 16:15:38
+ * @LastEditors: linxx
+ * @Description: 
+ * @FilePath: \page-bili\src\index.tsx
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './global.scss'
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <App />
+);
+
+
